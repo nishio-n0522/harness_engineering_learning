c8dedd5db4d5b55f6f855539aa6e1f7a767f88be
nisshi-o <101261714+nishio-n0522@users.noreply.github.com>
2026-04-30 06:55:53 +0900

    fix: lefthookを使用したガードレール実行の仕組みを追加

--- docs/phase1/exercise/3/scripts/check-commit-msg.sh
+++ docs/phase1/exercise/3/scripts/check-commit-msg.sh
@@ -3,7 +3,7 @@
 commit_message_file="$1"
 
 if ! grep -qE '^(feat|fix|docs|chore|refactor|test): .+' "$commit_message_file"; then
-	printf "\033[31mコミットメッセージがConventional Commits形式に従っていません\033[0m"
+	printf "\033[31mコミットメッセージがConventional Commits形式に従っていません\033[0m\n"
 	exit 1
 fi
 
