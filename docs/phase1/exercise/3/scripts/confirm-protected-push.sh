f1a8267557eeefddbd1bd091e01c0e6202d6804f
nisshi-o <101261714+nishio-n0522@users.noreply.github.com>
2026-04-30 21:31:42 +0900

    fix: pre-pushのhookを修正

--- docs/phase1/exercise/3/scripts/confirm-protected-push.sh
+++ docs/phase1/exercise/3/scripts/confirm-protected-push.sh
@@ -1,28 +1,12 @@
 #!/bin/sh
 
-has_main_or_master_branch(){
-	if echo "$1" | grep -qE '.+/(main|master)$'; then
-		return 0
-	fi
-	return 1
-}
-
-has_error=0
-while read local_ref local_sha remote_ref remote_sha; do
-	if has_main_or_master_branch "$remote_ref"; then
-		has_error=1
-	fi
-done
-
-if [ "$has_error" -eq 1 ]; then
-	printf "プッシュしようとしているリモートブランチが main もしくは master です。このままpushを継続しますか？問題なければ y を入力してください。"
-	read -r line < /dev/tty
-	if [ "$line" = "y" ]; then
-		exit 0
-	else
-		printf "\033[31minfo: プッシュをキャンセルしました\n\033[0m" 
-		exit 1
-	fi
-fi
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+case "$current_branch" in
+	main|master)
+		printf "プッシュしようとしているリモートブランチが main もしくは master です。このままpushを継続しますか？問題なければ y を入力してください。\n"
+		read -r line
+		[ "$line" = "y" ] || exit 1
+		;;
+esac
 
 exit 0
